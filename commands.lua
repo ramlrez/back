4bbe68f84df07c37ac02bc3f5f1fa74d43d88a3c
ramírez <65989098+ramlrez@users.noreply.github.com>
2023-04-21 15:07:37 -0600

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -6,21 +6,21 @@ function checkTarget(target)
 end
 return {
     kill = function(target,owner)
-        if not checkTarget() then return end
+        if not checkTarget(target) then return end
         if not pullOwner(owner) then return end
         pcall(function()
             game.Players.LocalPlayer.Character.Humanoid.Health = 0
         end)
     end,
     speak = function(target,owner, params)
-        if not checkTarget() then return end
+        if not checkTarget(target) then return end
         if not pullOwner(owner) then return end
         pcall(function()
             game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageReques:FireServer(table.concat(params,' '), 'All')
         end)
     end,
     bring = function(target,owner)
-        if not checkTarget() then return end
+        if not checkTarget(target) then return end
         if not pullOwner(owner) then return end
         pcall(function()
             local hrp = (pullOwner(owner).Character:FindFirstChild('HumanoidRootPart') and pullOwner().Character:FindFirstChild('HumanoidRootPart')) or (pullOwner().Character:FindFirstChild('Torso') and pullOwner().Character:FindFirstChild('Torso'))
