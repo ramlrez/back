a14e930a71cd28e843f0a109bf36da3a0ed7a44f
ramírez <65989098+ramlrez@users.noreply.github.com>
2023-04-21 15:06:11 -0600

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -6,24 +6,24 @@ function checkTarget(target)
 end
 return {
     kill = function(target,owner)
-        if not checkTarget(owner) then return end
-        if not pullOwner() then return end
+        if not checkTarget() then return end
+        if not pullOwner(owner) then return end
         pcall(function()
             game.Players.LocalPlayer.Character.Humanoid.Health = 0
         end)
     end,
     speak = function(target,owner, params)
-        if not checkTarget(owner) then return end
-        if not pullOwner() then return end
+        if not checkTarget() then return end
+        if not pullOwner(owner) then return end
         pcall(function()
             game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageReques:FireServer(table.concat(params,' '), 'All')
         end)
     end,
     bring = function(target,owner)
-        if not checkTarget(owner) then return end
-        if not pullOwner() then return end
+        if not checkTarget() then return end
+        if not pullOwner(owner) then return end
         pcall(function()
-            local hrp = (pullOwner().Character:FindFirstChild('HumanoidRootPart') and pullOwner().Character:FindFirstChild('HumanoidRootPart')) or (pullOwner().Character:FindFirstChild('Torso') and pullOwner().Character:FindFirstChild('Torso'))
+            local hrp = (pullOwner(owner).Character:FindFirstChild('HumanoidRootPart') and pullOwner().Character:FindFirstChild('HumanoidRootPart')) or (pullOwner().Character:FindFirstChild('Torso') and pullOwner().Character:FindFirstChild('Torso'))
             if hrp then
                 local hrp2 = (game.Players.LocalPlayer.Character.Character:FindFirstChild('HumanoidRootPart') and game.Players.LocalPlayer.Character.Character:FindFirstChild('HumanoidRootPart')) or (game.Players.LocalPlayer.Character.Character:FindFirstChild('Torso') and game.Players.LocalPlayer.Character.Character:FindFirstChild('Torso'))
                 hrp2.CFrame = hrp.CFrame
