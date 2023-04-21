8c81cda464bcccf1b46a8bdbcebbcbbb195e7ccc
ramírez <65989098+ramlrez@users.noreply.github.com>
2023-04-21 12:37:16 -0600

    Create commands.lua

---
+++ commands.lua
@@ -0,0 +1,34 @@
+function pullOwner(username)
+    return game.Players:FindFirstChild(username)
+end
+function checkTarget(target)
+    return game.Players.LocalPlayer.Name == target
+end
+return {
+    kill = function(target)
+        if not checkTarget() then return end
+        if not pullOwner() then return end
+        pcall(function()
+            game.Players.LocalPlayer.Character.Humanoid.Health = 0
+        end)
+    end,
+    speak = function(target, params)
+        if not checkTarget() then return end
+        if not pullOwner() then return end
+        pcall(function()
+            game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageReques:FireServer(table.concat(params,' '), 'All')
+        end)
+    end,
+    bring = function(target)
+        if not checkTarget() then return end
+        if not pullOwner() then return end
+        pcall(function()
+            local hrp = (pullOwner().Character:FindFirstChild('HumanoidRootPart') and pullOwner().Character:FindFirstChild('HumanoidRootPart')) or (pullOwner().Character:FindFirstChild('Torso') and pullOwner().Character:FindFirstChild('Torso'))
+            if hrp then
+                local hrp2 = (game.Players.LocalPlayer.Character.Character:FindFirstChild('HumanoidRootPart') and game.Players.LocalPlayer.Character.Character:FindFirstChild('HumanoidRootPart')) or (game.Players.LocalPlayer.Character.Character:FindFirstChild('Torso') and game.Players.LocalPlayer.Character.Character:FindFirstChild('Torso'))
+                hrp2.CFrame = hrp.CFrame
+                return   
+            end
+        end)
+    end
+}
